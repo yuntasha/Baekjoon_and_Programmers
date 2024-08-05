7ccf411c5b30b9ffe69ba81b2bbe17a4cdbaaeda
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-08-05 19:39:18 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4312 ms, Memory: 163348 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -13,62 +13,53 @@ public class Main {
 
         var N = Integer.parseInt(bufferedReader.readLine());
 
-        ArrayList<Integer>[] arr = new ArrayList[4];
-
-        for (int i=0; i<4; i++) arr[i] = new ArrayList<Integer>();
+        int[][] arr = new int[4][N];
 
         for (int i=0; i<N; i++) {
             var now = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            for (int j=0; j<4; j++) arr[j].add(now[j]);
+            for (int j=0; j<4; j++) arr[j][i] = now[j];
         }
 
         System.out.println(solution(N, arr));
     }
 
-    static long solution(int N, ArrayList<Integer>[] arr){
+    static long solution(int N, int[][] arr){
         var arr1 = new int[N*N];
-        var idx = 0;
+        var arr2 = new int[N*N];
 
+        var idx = 0;
         for (int i: arr[0]){
             for (int j: arr[1]){
                 arr1[idx++] = i+j;
             }
         }
-        Arrays.sort(arr1);
-        var result = 0L;
 
+        idx = 0;
         for (int i: arr[2]){
             for (int j: arr[3]){
-                var now = i+j;
-                result += findR(now, arr1) - findL(now , arr1);
+                arr2[idx++] = i+j;
+            }
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        var s = 0;
+        var e = arr2.length-1;
+        var result = 0L;
+        while (s<arr1.length && e>=0) {
+            if (arr1[s] + arr2[e] == 0) {
+                var cnt1 = 1L;
+                var cnt2 = 1L;
+                while (++s<arr1.length && arr1[s]+arr2[e]==0) cnt1++;
+                while (--e>=0 && arr1[s-1]+arr2[e]==0) cnt2++;
+                result += cnt1*cnt2;
+            } else if (arr1[s] + arr2[e] < 0) {
+                s++;
+            } else {
+                e--;
             }
         }
         return result;
-    }
-
-    static long findL(int n, int[] arr2){
-        if (arr2[0]==-n) return 0;
-        var s = 0;
-        var e = arr2.length-1;
-        while (s+1!=e){
-            var mid = (s+e)/2;
-            if (arr2[mid]+n<0) {
-                s = mid;
-            }else e = mid;
-        }
-        return e;
-    }
-
-    static long findR(int n, int[] arr2){
-        if (arr2[arr2.length-1]==-n) return arr2.length;
-        var s = 0;
-        var e = arr2.length-1;
-        while (s+1!=e){
-            var mid = (s+e)/2;
-            if (arr2[mid]+n<=0) {
-                s = mid;
-            }else e = mid;
-        }
-        return e;
     }
 }
