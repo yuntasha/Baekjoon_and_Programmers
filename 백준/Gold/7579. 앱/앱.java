0f667fde03dc7603fd3cd24075954d7adffd3bbc
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-08-06 22:05:34 +0900

    [Gold III] Title: 앱, Time: 128 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -5,8 +5,6 @@ import java.util.*;
 
 public class Main {
 
-    static int MAX_VALUE = 10_000_001;
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -25,15 +23,12 @@ public class Main {
 
     static int solution(int N, int M, int[] size, int[] cost){
         var costMaxSize = new int[10001];
-        var visited = new boolean[10001];
-        visited[0] = true;
         var minCost = 10000;
         for (int i=0; i<N; i++){
             for (int j=minCost-cost[i]; j>=0; j--) {
-                if (!visited[j]) continue;
+                if (j!=0 && costMaxSize[j]==0) continue;
                 costMaxSize[j+cost[i]] = Math.max(costMaxSize[j+cost[i]], costMaxSize[j]+size[i]);
                 if (costMaxSize[j+cost[i]]>=M) minCost=j+cost[i];
-                visited[j+cost[i]] = true;
             }
         }
         return minCost;
