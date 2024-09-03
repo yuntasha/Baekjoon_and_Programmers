d74a1526ee890c6533fd1df307252d8bf705d9ec
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-09-03 17:20:11 +0900

    [Gold III] Title: ACM Craft, Time: 1368 ms, Memory: 310192 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -12,51 +11,47 @@ public class Main {
 
         var T = Integer.parseInt(bufferedReader.readLine());
 
-        for (int i=0; i<T; i++){
-            var NM = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            var N = NM[0];
-            var M = NM[1];
+        for (int t=0; t<T; t++){
+            var NK = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            var N = NK[0];
+            var K = NK[1];
+
             var time = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-            var from = new ArrayList<HashSet<Integer>>();
-            var to = new ArrayList<HashSet<Integer>>();
-
-            for (int j=0; j<N; j++){
-                from.add(new HashSet<>());
-                to.add(new HashSet<>());
-            }
-            for (int j=0; j<M; j++){
-                var fromTo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(s -> Integer.parseInt(s)-1).toArray();
-                to.get(fromTo[0]).add(fromTo[1]);
-                from.get(fromTo[1]).add(fromTo[0]);
+            Set<Integer>[] before = new Set[N];
+            Set<Integer>[] after = new Set[N];
+            for (int n=0; n<N; n++) {
+                before[n] = new HashSet<Integer>();
+                after[n] = new HashSet<Integer>();
             }
 
-            var dest = Integer.parseInt(bufferedReader.readLine())-1;
+            for (int k=0; k<K; k++) {
+                var now = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(s -> Integer.parseInt(s)-1).toArray();
+                before[now[1]].add(now[0]);
+                after[now[0]].add(now[1]);
+            }
 
-            System.out.println(solution(N, dest, time, from, to));
+            var win = Integer.parseInt(bufferedReader.readLine())-1;
+            System.out.println(solution(N, K, time, before, after, win));
         }
     }
 
-    static int solution(int N, int dest, int[] time, ArrayList<HashSet<Integer>> from, ArrayList<HashSet<Integer>> to){
-        var q = new LinkedList<Integer>();
+    static int solution(int N, int K, int[] time, Set<Integer>[] before, Set<Integer>[] after, int win){
         var result = new int[N];
-
-        for (int i=0; i<N; i++){
-            if (from.get(i).isEmpty()) {
-                q.add(i);
-                result[i] = time[i];
-            }
+        var q = new LinkedList<Integer>();
+        for (int i=0; i<N; i++) {
+            if (before[i].isEmpty()) q.add(i);
         }
 
-        while (!q.isEmpty()){
+        while (!q.isEmpty()) {
             var now = q.remove();
-            for (int i: to.get(now)){
-                result[i] = Math.max(result[i], result[now] + time[i]);
-                from.get(i).remove(now);
-                if (from.get(i).isEmpty()) q.add(i);
+            for (int n: after[now]){
+                result[n] = Math.max(result[n], result[now] + time[now]);
+                before[n].remove(now);
+                if (before[n].isEmpty()) q.add(n);
             }
         }
 
-        return result[dest];
+        return result[win] + time[win];
     }
 }
