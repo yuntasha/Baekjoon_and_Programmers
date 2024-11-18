357fb29a9eaca2612ede1e5579d70a1ab32adb09
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-11-18 18:20:46 +0900

    [Gold IV] Title: 충전하기, Time: 1904 ms, Memory: 407432 KB -BaekjoonHub

--- 백준/Gold/32387. 충전하기/충전하기.java
+++ 백준/Gold/32387. 충전하기/충전하기.java
@@ -5,9 +5,6 @@ import java.util.*;
 
 public class Main {
 
-    static TreeSet<Integer> set;
-    static int[] log;
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
@@ -15,39 +12,111 @@ public class Main {
         var N = NM[0];
         var T = NM[1];
 
-        set = new TreeSet<Integer>();
-        log = new int[N+1];
-
-        for (int i=1; i<=N; i++) set.add(i);
+        var segTree = new SegTree(N);
 
         StringJoiner sb = new StringJoiner("\n");
 
         for (int t = 1; t <= T; t++) {
-            var input = bf.readLine().split(" ");
-            sb.add(String.valueOf(solution(t, Integer.parseInt(input[0]), Integer.parseInt(input[1]))));
+            var input = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            sb.add(String.valueOf(solution(t, input[0], input[1], segTree)));
         }
 
         System.out.println(sb.toString());
     }
 
-    static int solution(int t, int type, int port) {
-        if (type == 1) {
-            Integer result = set.ceiling(port);
-
-            if (Objects.isNull(result)) {
-                return -1;
-            }
-
-            set.remove(result);
-            log[result] = t;
-
-            return result;
+    static int solution(int t, int type, int port, SegTree segTree) {
+        if (type==1) {
+            return segTree.usePort(t, port);
         } else {
-            if (!set.contains(port)) {
-                set.add(port);
-                return log[port];
+            return segTree.unUsedPort(port);
+        }
+    }
+
+    static class SegTree{
+        int[] nodes;
+        int range;
+        int[] log;
+
+        SegTree(int N) {
+            range = N;
+            var ncnt = 1;
+            while (ncnt<range) ncnt*=2;
+            ncnt*=2;
+            nodes = new int[ncnt];
+            log = new int[ncnt];
+            createNode(1, 1, N);
+        }
+
+        int createNode(int idx, int start, int end) {
+            if (start==end) {
+                nodes[idx] = 1;
+            } else {
+                var mid = (start + end) / 2;
+                nodes[idx] = createNode(idx*2, start, mid) + createNode(idx*2+1, mid+1, end);
             }
-            return -1;
+            return nodes[idx];
+        }
+
+        int usePort(int t, int num) {
+            var result = findAndUsePort(t, num, 1, 1, range);
+            if (result==0) return -1;
+            return result;
+        }
+
+        int findAndUsePort(int t, int num, int idx, int start, int end) {
+            var mid = (start + end) / 2;
+            if (start==end) {
+                if (nodes[idx]==0) {
+                    return 0;
+                }
+                log[idx] = t;
+                nodes[idx] = 0;
+                return end;
+            } else {
+                if (nodes[idx]==0) {
+                    return 0;
+                }
+                if (mid>=num) {
+                    var result = findAndUsePort(t, num, idx * 2, start, mid);
+                    if (result > 0) {
+                        nodes[idx]--;
+                        return result;
+                    }
+                }
+
+                var result = findAndUsePort(t, num, idx * 2 + 1, mid + 1, end);
+                if (result > 0) {
+                    nodes[idx]--;
+                    return result;
+                }
+            }
+            return 0;
+        }
+
+        int unUsedPort(int num) {
+            return findUsedPort(num, 1, 1, range);
+        }
+
+        int findUsedPort(int num, int idx, int start, int end) {
+            if (start==end) {
+                if (nodes[idx] == 1) {
+                    return -1;
+                }
+                nodes[idx] = 1;
+                return log[idx];
+            } else {
+                var mid = (start+end)/2;
+                int result;
+                if (num>mid) {
+                    result = findUsedPort(num, idx*2+1, mid+1, end);
+                } else {
+                    result = findUsedPort(num, idx*2, start, mid);
+                }
+                if (result!=-1) {
+                    nodes[idx]++;
+                }
+                return result;
+            }
         }
     }
 }
