8cba55d7fc5410901675e63883d65c824ba0ff52
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-12-05 16:11:29 +0900

    [Gold II] Title: 부분 문자열 뽑기 게임, Time: 156 ms, Memory: 15112 KB -BaekjoonHub

--- 백준/Gold/1519. 부분 문자열 뽑기 게임/부분 문자열 뽑기 게임.java
+++ 백준/Gold/1519. 부분 문자열 뽑기 게임/부분 문자열 뽑기 게임.java
@@ -1,37 +1,34 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        var bf = new BufferedReader(new InputStreamReader(System.in));
 
-        var S = bf.readLine();
+        var S = read();
 
         System.out.println(solution(S));
     }
 
-    static int solution(String S) {
-        var N = Integer.parseInt(S);
+    static int solution(int N) {
         var fail = new boolean[N];
 
         if (N < 10) {
             return -1;
         }
 
-        for (int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             fail[i] = true;
         }
 
-        for (int n=10; n<N; n++) {
+        for (int n = 10; n < N; n++) {
 
             var isWin = false;
 
-            var iMax = (n<<3)+(n<<1);
+            var iMax = (n << 3) + (n << 1);
 
-            Loop : for (int i=10; i<=iMax; i=(i<<3) + (i<<1)) {
-                for (int now = n%i; now>0; now/=10) {
+            Loop:
+            for (int i = 10; i <= iMax; i = (i << 3) + (i << 1)) {
+                for (int now = n % i; now > 0; now /= 10) {
                     if (now == n) continue;
                     if (fail[n - now]) {
                         isWin = true;
@@ -47,8 +44,8 @@ public class Main {
 
         var result = Integer.MAX_VALUE;
 
-        for (int i=10; i<=N*10; i*=10) {
-            for (int now = N%i; now>0; now/=10) {
+        for (int i = 10; i <= N * 10; i *= 10) {
+            for (int now = N % i; now > 0; now /= 10) {
                 if (now == N) continue;
                 if (fail[N - now]) {
                     result = Math.min(result, now);
@@ -56,6 +53,13 @@ public class Main {
             }
         }
 
-        return result==Integer.MAX_VALUE?-1:result;
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+
+    static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
