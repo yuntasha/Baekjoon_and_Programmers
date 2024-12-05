90933e4a9b6a533b85ac8fc900932867c6617f57
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-12-05 19:36:03 +0900

    [Gold II] Title: 부분 문자열 뽑기 게임, Time: 108 ms, Memory: 12528 KB -BaekjoonHub

--- 백준/Gold/1519. 부분 문자열 뽑기 게임/부분 문자열 뽑기 게임.java
+++ 백준/Gold/1519. 부분 문자열 뽑기 게임/부분 문자열 뽑기 게임.java
@@ -1,21 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Arrays;
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        String S = bf.readLine();
-
-        System.out.println(solution(S));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(Integer.parseInt(br.readLine())));
     }
-
-    static int solution(String S) {
-        int N = Integer.parseInt(S);
+    public static int solution(int N) {
         boolean[] fail = new boolean[N];
-
         if (N < 10) {
             return -1;
         }
@@ -23,28 +16,10 @@ public class Main {
         for (int i=0; i<10; i++) {
             fail[i] = true;
         }
-
         for (int n=10; n<N; n++) {
-
-            boolean isWin = false;
-
-            int iMax = (n<<3)+(n<<1);
-
-            Loop : for (int i=10; i<=iMax; i=(i<<3) + (i<<1)) {
-                for (int now = n%i; now>0; now/=10) {
-                    if (now == n) continue;
-                    if (fail[n - now]) {
-                        isWin = true;
-                        break Loop;
-                    }
-                }
-            }
-
-            fail[n] = !isWin;
+            fail[n] = !solve(n, (n << 3) + (n << 1), fail);
         }
-
         int result = Integer.MAX_VALUE;
-
         for (int i=10; i<=((N<<3) + (N<<1)); i=(i<<3) + (i<<1)) {
             for (int now = N%i; now>0; now/=10) {
                 if (now == N) continue;
@@ -53,7 +28,18 @@ public class Main {
                 }
             }
         }
-
         return result==Integer.MAX_VALUE?-1:result;
+    }
+    
+    public static boolean solve(int n, int iMax, boolean[] fail) {
+    	for (int i=10; i<=iMax; i=(i<<3) + (i<<1)) {
+            for (int now = n%i; now>0; now/=10) {
+                if (now == n) continue;
+                if (fail[n - now]) {
+                    return true;
+                }
+            }
+        }
+    	return false;
     }
 }
