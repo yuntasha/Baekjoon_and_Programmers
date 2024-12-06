2fd8ac4659adbb173df6eccb141236f8e6141737
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-12-06 21:54:45 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 192 ms, Memory: 22668 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
@@ -2,20 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        var bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        var N = Integer.parseInt(bf.readLine());
+        int N = Integer.parseInt(bf.readLine());
 
-        var st = new StringTokenizer(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        var arr = new ArrayList<String>();
+        List<String> arr = new ArrayList<String>();
 
         for (int i=0; i<N; i++) {
             arr.add(st.nextToken());
@@ -27,13 +26,13 @@ public class Main {
     static String solution(int N, List<String> arr) {
         arr.sort(Main::compare);
 
-        var result = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
         for (String s : arr) {
             result.append(s);
         }
 
-        var s = 0;
+        int s = 0;
         while (s<N && result.charAt(s) == '0') {
             s++;
         }
