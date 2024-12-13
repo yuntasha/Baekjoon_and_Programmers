4a93bc6b8847535876483fab4e359cb975103e9a
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-12-13 16:43:37 +0900

    [Gold V] Title: 도미노, Time: 116 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Gold/1552. 도미노/도미노.java
+++ 백준/Gold/1552. 도미노/도미노.java
@@ -21,31 +21,31 @@ public class Main {
             }
         }
 
-        System.out.println(solution(N, arr).stream().map(String::valueOf).collect(Collectors.joining("\n")));
+        System.out.println(Arrays.stream(solution(N, arr)).mapToObj(String::valueOf).collect(Collectors.joining("\n")));
     }
 
-    static List<Integer> solution(int N, int[][] arr) {
-        List<Integer> list = new ArrayList<>();
-        dfs(N, arr, 0, 1, 0, list, IntStream.range(0, N).toArray(), 0);
+    static int[] solution(int N, int[][] arr) {
+        int[] result = new int[2];
+        result[0] = Integer.MAX_VALUE;
+        result[1] = Integer.MIN_VALUE;
 
-        List<Integer> result = new ArrayList<>();
-        list.sort(Comparator.naturalOrder());
-        result.add(list.get(0));
-        result.add(list.get(list.size()-1));
+        dfs(N, arr, 0, 1, 0, result, IntStream.range(0, N).toArray(), 0);
 
         return result;
     }
 
-    static void dfs(int N, int[][] arr, int now, int point, int visited, List<Integer> list, int[] group, int cycles) {
+    static void dfs(int N, int[][] arr, int now, int point, int visited, int[] result, int[] group, int cycles) {
         if (now == N) {
-            list.add(point * ((cycles & 1) > 0 ? 1 : -1));
+            int n = point * ((cycles & 1) > 0 ? 1 : -1);
+            result[0] = Math.min(result[0], n);
+            result[1] = Math.max(result[1], n);
             return;
         }
 
         for (int i=0; i<N; i++) {
             if ((visited & (1<<i)) > 0) continue;
             if (i==now) {
-                dfs(N, arr, now + 1, point * arr[now][i], visited | (1 << i), list, group, cycles + 1);
+                dfs(N, arr, now + 1, point * arr[now][i], visited | (1 << i), result, group, cycles + 1);
                 continue;
             }
             int[] nextGroup = Arrays.copyOf(group, N);
@@ -53,11 +53,11 @@ public class Main {
             int ng = findG(now, nextGroup);
 
             if (ig == ng) {
-                dfs(N, arr, now + 1, point * arr[now][i], visited | (1 << i), list, nextGroup, cycles + 1);
+                dfs(N, arr, now + 1, point * arr[now][i], visited | (1 << i), result, nextGroup, cycles + 1);
                 continue;
             }
             nextGroup[Math.max(ig, ng)] = Math.min(ig, ng);
-            dfs(N, arr, now + 1, point * arr[now][i], visited | (1 << i), list, nextGroup, cycles);
+            dfs(N, arr, now + 1, point * arr[now][i], visited | (1 << i), result, nextGroup, cycles);
         }
     }
 
