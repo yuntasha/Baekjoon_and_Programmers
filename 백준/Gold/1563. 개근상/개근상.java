e75169a014022d5e91c02748b29a8bbf7e1223a8
yuntasha <97961558+yuntasha@users.noreply.github.com>
2024-12-15 21:20:16 +0900

    [Gold IV] Title: 개근상, Time: 68 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Gold/1563. 개근상/개근상.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int MAX_VALUE = 1_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        System.out.println(solution(N));
+    }
+
+    static int solution(int N) {
+        int[][][] dp = new int[N][3][2];
+
+        dp[0][0][0] = 1;
+        dp[0][1][0] = 1;
+        dp[0][0][1] = 1;
+
+        for (int n=1; n<N; n++) {
+            dp[n][0][0] = (dp[n-1][0][0] + dp[n-1][1][0] + dp[n-1][2][0]) % MAX_VALUE;
+            dp[n][1][0] = dp[n-1][0][0];
+            dp[n][2][0] = dp[n-1][1][0];
+            dp[n][0][1] = (dp[n-1][0][1] + dp[n-1][1][1] + dp[n-1][2][1] + dp[n][0][0]) % MAX_VALUE;
+            dp[n][1][1] = dp[n-1][0][1];
+            dp[n][2][1] = dp[n-1][1][1];
+        }
+
+        int result = 0;
+
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<2; j++) {
+                result += dp[N-1][i][j];
+                result %= MAX_VALUE;
+            }
+        }
+
+        return result;
+    }
+}
