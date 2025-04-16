d68cb49349c836b1bf9decdeb66e7cfae9423b60
yuntasha <97961558+yuntasha@users.noreply.github.com>
2025-04-16 22:41:23 +0900

    [Gold III] Title: ACM Craft, Time: 668 ms, Memory: 237728 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,57 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+/*
+저 문제에서 이제 dfs로 해결을 해봐요
+그럼 어떻게하냐
+
+W를 건설 걸리는 시간
+W를 건설하기 위해 필요한 건물
+그 건물도 시간을 구해야겠죠?
+ */
+
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bf.readLine());
 
-        var T = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder output = new StringBuilder();
 
-        for (int t=0; t<T; t++){
-            var NK = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            var N = NK[0];
-            var K = NK[1];
+        for (int t = 0; t < T; t++) {
+            StringTokenizer input = new StringTokenizer(bf.readLine());
+            int N = Integer.parseInt(input.nextToken());
+            int K = Integer.parseInt(input.nextToken());
 
-            var time = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            List<List<Integer>> before = new ArrayList<>();
 
-            Set<Integer>[] before = new Set[N];
-            Set<Integer>[] after = new Set[N];
-            for (int n=0; n<N; n++) {
-                before[n] = new HashSet<Integer>();
-                after[n] = new HashSet<Integer>();
+            for (int i = 0; i < N; i++) {
+                before.add(new ArrayList<>());
             }
 
-            for (int k=0; k<K; k++) {
-                var now = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(s -> Integer.parseInt(s)-1).toArray();
-                before[now[1]].add(now[0]);
-                after[now[0]].add(now[1]);
+            int[] cost = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            for (int i = 0; i < K; i++) {
+                input = new StringTokenizer(bf.readLine());
+
+                int a = Integer.parseInt(input.nextToken()) - 1;
+                int b = Integer.parseInt(input.nextToken()) - 1;
+
+                before.get(b).add(a);
             }
 
-            var win = Integer.parseInt(bufferedReader.readLine())-1;
-            System.out.println(solution(N, K, time, before, after, win));
+            int W = Integer.parseInt(bf.readLine()) - 1;
+
+            output.append("\n").append(solution(N, K, before, cost, W));
         }
+
+        System.out.println(output.substring(1));
     }
 
-    static int solution(int N, int K, int[] time, Set<Integer>[] before, Set<Integer>[] after, int win){
-        var result = new int[N];
-        var q = new LinkedList<Integer>();
-        for (int i=0; i<N; i++) {
-            if (before[i].isEmpty()) q.add(i);
+    static int solution(int N, int K, List<List<Integer>> before, int[] cost, int W) {
+        int[] dp = new int[N];
+        boolean[] visited = new boolean[N];
+        return find(before, cost, W, dp, visited);
+    }
+
+    static int find(List<List<Integer>> before, int[] cost, int w, int[] dp, boolean[] visited) {
+        if (visited[w]) {
+            return dp[w];
         }
 
-        while (!q.isEmpty()) {
-            var now = q.remove();
-            for (int n: after[now]){
-                result[n] = Math.max(result[n], result[now] + time[now]);
-                before[n].remove(now);
-                if (before[n].isEmpty()) q.add(n);
-            }
+        visited[w] = true;
+        dp[w] = 0;
+
+        for (int i : before.get(w)) {
+            dp[w] = Math.max(dp[w], find(before, cost, i, dp, visited));
         }
 
-        return result[win] + time[win];
+        // time은 현재 건물을 짓기위한 준비시간
+        dp[w] = dp[w] + cost[w];
+        return dp[w];
     }
 }
