2c4640799cd216529c6c556011b46af42de51452
yuntasha <97961558+yuntasha@users.noreply.github.com>
2025-05-31 20:49:18 +0900

    [Gold IV] Title: 2로 몇 번 나누어질까, Time: 112 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+그냥 깡으로 해버리면 무조건 시간초과임
+그러니까 16까지 16은 1개가 나옴
+2 4 6 8 10 12
+절반만 사용
+/2하고 그걸 전체에서 빼줌
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer input = new StringTokenizer(bf.readLine());
+        long A = Long.parseLong(input.nextToken());
+        long B = Long.parseLong(input.nextToken());
+        System.out.println(solution(A, B));
+    }
+
+    public static long solution(long A, long B) {
+        return getSum(B) - getSum(A - 1);
+    }
+
+    public static long getSum(long N) {
+        long result = 0;
+        long now = 1;
+
+        while (now <= N) {
+            result += ((N / now) - (N / now / 2)) * now;
+            now *= 2;
+        }
+
+        return result;
+    }
+}
