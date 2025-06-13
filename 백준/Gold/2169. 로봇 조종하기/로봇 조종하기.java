c09ea454a3a76c896a0ecab393c01202a01b5aa3
yuntasha <97961558+yuntasha@users.noreply.github.com>
2025-06-13 16:00:32 +0900

    [Gold II] Title: 로봇 조종하기, Time: 712 ms, Memory: 105148 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -1,12 +1,11 @@
 /*
-홀짝 기준으로 교환 가능
-실제 정렬해보자
-그리고 홀짝 위치 바뀐거 체크하자
-합이였네
 1000 * 1000이잖아
 그러면 1_000_000_000인데
 개오바임
-
+이거 카데인 알고리즘이랑 비슷하네
+앞에부터 따라오는게 최대냐 아니면 위에서 바로 내려오는게 최대냐 이거로 dp 한번 더 엮어주는 문제
+구간 합이라고 생각하면 편함
+특정 위치에서 끝나는 구간합 중 최댓값 구하는 문제
  */
 
 import java.io.*;
@@ -32,26 +31,32 @@ public class Main {
     }
 
     static int solution(int N, int M, int[][] map) {
-        int[][] dp = new int[N][M];
+        int[] result = new int[M];
 
-        dp[0][0] = map[0][0];
+        result[0] = map[0][0];
 
         for (int i = 1; i < M; i++) {
-            dp[0][i] = map[0][i] + dp[0][i - 1];
+            result[i] = map[0][i] + result[i - 1];
         }
 
-        for (int i = 0; i < N - 1; i++) {
-            Arrays.fill(dp[i + 1], Integer.MIN_VALUE);
-            for (int s = 0; s < M; s++) {
-                int now = 0;
-                for (int e = s; e < M; e++) {
-                    now += map[i + 1][e];
-                    dp[i +  1][e] = Math.max(dp[i + 1][e], dp[i][s] + now);
-                    dp[i +  1][s] = Math.max(dp[i + 1][s], dp[i][e] + now);
-                }
+        for (int i = 1; i < N; i++) {
+            int[] r = new int[M];
+            r[0] = result[0] + map[i][0];
+            for (int j = 1; j < M; j++) {
+                r[j] = map[i][j] + Math.max(r[j - 1], result[j]);
+            }
+
+            int[] l = new int[M];
+            l[M - 1] = result[M - 1] + map[i][M - 1];
+            for (int j = M - 2; j >= 0; j--) {
+                l[j] = map[i][j] + Math.max(l[j + 1], result[j]);
+            }
+
+            for (int j = 0; j < M; j++) {
+                result[j] = Math.max(l[j], r[j]);
             }
         }
 
-        return dp[N - 1][M - 1];
+        return result[M - 1];
     }
 }
