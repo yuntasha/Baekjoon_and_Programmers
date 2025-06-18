37c7654a38576b7350ebca0c33e1fcf13d21570c
yuntasha <97961558+yuntasha@users.noreply.github.com>
2025-06-18 12:30:15 +0900

    [Gold II] Title: 합리적인 이동경로, Time: 268 ms, Memory: 20188 KB -BaekjoonHub

--- 백준/Gold/2176. 합리적인 이동경로/합리적인 이동경로.java
+++ 백준/Gold/2176. 합리적인 이동경로/합리적인 이동경로.java
@@ -10,28 +10,22 @@
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf =new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer input = new StringTokenizer(bf.readLine());
-
-        int N = Integer.parseInt(input.nextToken());
-        int M = Integer.parseInt(input.nextToken());
+        int N = read();
+        int M = read();
 
         List<List<Line>> lines = new ArrayList<>();
 
         for (int i = 0; i <= N; i++) lines.add(new ArrayList<>());
 
         for (int i = 0; i < M; i++) {
-            input = new StringTokenizer(bf.readLine());
-
-            int a = Integer.parseInt(input.nextToken());
-            int b = Integer.parseInt(input.nextToken());
-            int c = Integer.parseInt(input.nextToken());
+            int a = read();
+            int b = read();
+            int c = read();
 
             lines.get(a).add(new Line(b, c));
             lines.get(b).add(new Line(a, c));
@@ -103,5 +97,16 @@ public class Main {
             this.dest = dest;
             this.len = len;
         }
+    }
+
+    static int read() throws IOException {
+        int n = 0;
+        int c;
+
+        while ((c = System.in.read()) >= '0') {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        return n;
     }
 }
