c8a1f596cb67bb24144b0cfd76760f2a082f117a
yuntasha <97961558+yuntasha@users.noreply.github.com>
2025-07-08 15:28:23 +0900

    [Gold IV] Title: 이진수, Time: 120 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Gold/2226. 이진수/이진수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        System.out.print(solution(N));
+    }
+
+    public static String solution(int N) {
+        BigInteger result = BigInteger.ZERO;
+
+        for (int i = 2; i <= N; i++) {
+            result = result.multiply(BigInteger.TWO);
+            if ((i & 1) == 1) result = result.subtract(BigInteger.ONE);
+            else result = result.add(BigInteger.ONE);
+        }
+
+        return result.toString();
+    }
+}
