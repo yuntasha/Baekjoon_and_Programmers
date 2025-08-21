96132bf1db625e6a4fe99293bdfc9ae615a690d2
yuntasha <97961558+yuntasha@users.noreply.github.com>
2025-08-21 16:32:02 +0900

    [level 2] Title: 최솟값 만들기, Time: 6.29 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        return IntStream.range(0, A.length).map(i -> A[i] * B[A.length - i - 1]).sum();
+    }
+}
