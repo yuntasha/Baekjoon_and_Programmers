a05c589212cb5e4a46adee01f39bffe3a5cc862b
yuntasha <97961558+yuntasha@users.noreply.github.com>
2025-09-03 16:09:59 +0900

    [Gold V] Title: 보물섬, Time: 440 ms, Memory: 173100 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,88 +1,104 @@
 import java.io.*;
 import java.util.*;
 
-/*
-그냥 전체 위치에서 갈 수 있는 최장거리 뽑아서 가져오자
- */
 
 public class Main {
 
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int r;
+    static int c;
+    static boolean[][] visited;
+    static int[][] mapp;
+    static int maxTime = 0;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer input = new StringTokenizer(bf.readLine());
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(input.nextToken());
-        int M = Integer.parseInt(input.nextToken());
+        st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[N][M];
+        mapp = new int[r][c];
+        for (int i=0; i<r; i++) {
 
-        for (int i = 0; i < N; i++) {
-            char[] cs = bf.readLine().toCharArray();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = cs[j] == 'L' ? 1 : 0;
+            String[] row = br.readLine().split("");
+
+            for (int j=0; j<c; j++) {
+
+                String s = row[j];
+                mapp[i][j] = s.equals("W") ? 0 : 1;
+
+            }
+
+        }
+
+        // bfs
+        for (int i=0; i<r; i++) {
+            for (int j=0; j<c; j++) {
+                if (mapp[i][j] == 0) continue;
+                visited = new boolean[r][c];
+                bfs(new Node(i, j, 0));
+
             }
         }
 
-        System.out.println(solution(N, M, map));
+        // result
+        System.out.print(maxTime);
+
     }
 
-    public static int solution(int N, int M, int[][] map) {
-        int result = 0;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                result = Math.max(result, getDist(i, j, N, M, map));
-            }
-        }
 
-        return result;
-    }
+    static void bfs(Node stt) {
 
-    public static int getDist(int n, int m, int N, int M, int[][] map) {
-        if (map[n][m] == 0) return 0;
-
-        int result = 0;
-        ArrayDeque<Node> q = new ArrayDeque<>();
-        int[][] dist = new int[N][M];
-
-        dist[n][m] = 1;
-        q.add(new Node(n, m));
+        Deque<Node> q = new ArrayDeque<>();
+        q.add(stt);
+        visited[stt.r][stt.c] = true;
 
         while (!q.isEmpty()) {
-            Node now = q.remove();
 
-            result = dist[now.n][now.m];
+            Node cur = q.pop();
 
-            for (int d = 0; d < 4; d++) {
-                int x = now.n + dx[d];
-                int y = now.m + dy[d];
+            // 상하좌우 탐색
+            for (int i=0; i<4; i++) {
 
-                if (!isIn(x, y, N, M)) continue;
-                if (map[x][y] == 0) continue;
-                if (dist[x][y] > 0) continue;
-                dist[x][y] = dist[now.n][now.m] + 1;
-                q.add(new Node(x, y));
+                int nr = cur.r + dr[i];
+                int nc = cur.c + dc[i];
+
+                if (!(0<=nr && nr<r) || !(0<=nc && nc<c)) continue;
+                if (visited[nr][nc]) continue;
+                if (mapp[nr][nc] == 0) continue;
+
+                visited[nr][nc] = true;
+                q.add(new Node(nr, nc, cur.t+1));
+                maxTime = Math.max(maxTime, cur.t+1);
+
             }
         }
-
-        return result - 1;
     }
 
-    static boolean isIn(int n, int m, int N, int M) {
-        return 0 <= n && n < N && 0 <= m && m < M;
-    }
+
 
     static class Node {
-        int n;
-        int m;
 
-        public Node(int n, int m) {
-            this.n = n;
-            this.m = m;
+        int r;
+        int c;
+        int t;
+
+        Node(int r, int c, int t) {
+            this.r = r;
+            this.c = c;
+            this.t = t;
         }
+
     }
+
+
+
 }
