bab8a68561e22fe48a9bb4beb1c226f047ce7226
yuntasha <97961558+yuntasha@users.noreply.github.com>
2025-09-11 22:33:02 +0900

    [Silver III] Title: 안테나, Time: 516 ms, Memory: 42776 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.java
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -1,28 +1,41 @@
+/*
+N = 4
+2번째 값을 가져와야한다
+인덱스 = 1
+짝수면 N / 2 - 1
+
+N = 5
+3번째 값을 가져와야 한다
+인덱스 2
+홀수면 N / 2
+ */
+
 import java.io.*;
 import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
 
         int N = Integer.parseInt(bf.readLine());
 
-        int[] x = new int[N];
-
         StringTokenizer input = new StringTokenizer(bf.readLine());
 
-        for (int i = 0; i < N; i++) {
-            x[i] = Integer.parseInt(input.nextToken());
-        }
+        int[] arr = new int[N];
 
-        System.out.println(solution(N, x));
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(input.nextToken());
+
+        System.out.println(solution(N, arr));
     }
 
-    public static long solution(int N, int[] x) {
-        Arrays.sort(x);
+    public static int solution(int N, int[] arr) {
+        Arrays.sort(arr);
 
-        return x[(N >> 1) - (N & 1 ^ 1)];
+        if (N % 2 == 1) return arr[N / 2];
+
+        return arr[N / 2 - 1];
     }
 }
